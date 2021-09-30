c7487fafeb5cece5e2f60f3d02c6dbfec05c68f3
mschneider30 <mschneider30@stuy.edu>
2021-09-30 18:54:52 -0400

    Added 08_hellofxn

---
+++ 08_hellofxn/greet.java
@@ -0,0 +1,8 @@
+public class greet {
+	public static void main(String[] args) {
+		System.out.println("No hablo queso");
+	}
+	public static greet(String) {
+		return "Why hello there, what is happening?";
+	}
+}
