96a646ab2de7265c9e41c1f9ac31e9d6faa14a3f
Max Schneider <mschneider30@stuy.edu>
2021-09-30 19:01:09 -0400

    Update greet.java

--- 08_hellofxn/greet.java
+++ 08_hellofxn/greet.java
@@ -3,13 +3,13 @@ Thusly:
 void greet(string) goes not work. You need static to void greet. TYou also need to specify variable types.
 */
 
-public class Greet {
+public class greet {
 	static void greet(String s) {
 		System.out.println(s);
 	}
 	public static void main(String[] args) {
 		greet("My My, what a surprise");
 		System.out.println();
-		Greet();
+		greet();
 	}
 }
