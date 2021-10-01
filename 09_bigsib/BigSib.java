53c7c66e601b3e0f03dd96a5ea81dd5050ab3987
mschneider30 <mschneider30@stuy.edu>
2021-10-01 13:09:39 -0400

    Added 09_BigSib

---
+++ 09_bigsib/BigSib.java
@@ -0,0 +1,9 @@
+/* I discovered how to get methods from other classes. This was helpful because it allows fro multiple clases to contrbute to one thing
+Additionally, I discovered the importance of syntax after getting many different errors and interpreting what I did wrong
+*/
+
+public class BigSib {
+	static void greet(String s) {
+		System.out.println(s);
+	}
+}
---
+++ 09_bigsib/greet.java
@@ -0,0 +1,5 @@
+public class greet {
+	public static void main(String[] args) {
+		BigSib.greet("My My, what a surprise");
+	}
+}
