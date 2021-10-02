1f06401e8e1981b12e1f7ba11259b5e93d047f52
Max Schneider <mschneider30@stuy.edu>
2021-10-02 11:28:57 -0400

    Update Date.java

--- pre/02/Date.java
+++ pre/02/Date.java
@@ -1,4 +1,4 @@
-public class Time {
+public class Date {
     public static void main(String[] args) {
       String day = "Wednesday";
       int date = 11;
@@ -22,15 +22,4 @@ public class Time {
       System.out.println(year);
       
       }
-  public static void main(String[] args) {
-    int seconds = 30;
-    int minutes = 48;
-    int hours = 15;
-    System.out.print("Number of seconds since midnight:");
-    System.out.println(seconds + minutes * 60 + hours *60*60);
-    System.out.print("Number of seconds until midnight:");
-    System.out.println(60*60*24-seconds + minutes * 60 + hours *60*60);
-    System.out.print("Percentage of day passed:"); 
-    System.out.println((seconds + minutes * 60 + hours *60*60)/(60*60*24));
-    }
     }
