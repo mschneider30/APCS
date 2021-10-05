f935b1643399be6189de4f78ccaa2806613d4a0f
Max Schneider <mschneider30@stuy.edu>
2021-10-04 21:04:15 -0400

    Update greet.java

--- 09_bigsib/greet.java
+++ 09_bigsib/greet.java
@@ -1,3 +1,10 @@
+/*
+Max Schneider
+APCS
+HW 9 -- What Are BiSibs Good For/BigSib.java/ created BigSib.java file
+2021 10 04
+*/
+
 public class greet {
 	public static void main(String[] args) {
 		BigSib.greet("Soup");
