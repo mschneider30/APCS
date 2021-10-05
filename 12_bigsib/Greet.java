fd884083249c06793a4d8d58a1c1b7fd15135ac8
Max Schneider <mschneider30@stuy.edu>
2021-10-05 18:53:08 -0400

    Update Greet.java

--- 12_bigsib/Greet.java
+++ 12_bigsib/Greet.java
@@ -6,6 +6,34 @@ HW 12 -- On Elder Individuality And the Elimination of Radio Fuzz/BigSib.java/ch
 */
 
 public class Greet {
-	
+	public static void main( String[] args ) {
+    		String greeting;
+
+   		BigSib richard = new BigSib();
+    		richard.setHelloMsg("Word up");
+
+    		greeting = richard.greet("freshman");
+    		System.out.println(greeting);
+		
+		
+   		BigSib richard1 = new BigSib();
+    		richard1.setHelloMsg("Salutations");
+
+    		greeting = richard1.greet("Dr. Spaceman");
+    		System.out.println(greeting);
+		
+		
+   		BigSib richard2 = new BigSib();
+    		richard2.setHelloMsg("Hey ya");
+
+    		greeting = richard2.greet("Kong Fooey");
+    		System.out.println(greeting);
+		
+		
+   		BigSib richard3 = new BigSib();
+    		richard3.setHelloMsg("Sup");
+
+    		greeting = richard3.greet("mom");
+    		System.out.println(greeting);
 	}
 }
