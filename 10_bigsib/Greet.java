e639ab52466df53cd7e1b39cf8f1ae7b9cd30ca4
Max Schneider <mschneider30@stuy.edu>
2021-10-04 21:06:01 -0400

    Update Greet.java

--- 10_bigsib/Greet.java
+++ 10_bigsib/Greet.java
@@ -1,3 +1,10 @@
+/*
+Max Schneider
+APCS
+HW 10 -- Refactor Big Sib One/BigSib.java/changed Greet.java file
+2021 10 05
+*/
+
 public class Greet {
 	public static void main(String[] args) {
 		System.out.println(BigSib.greet("Soup"));
