97d77b27f6beb1104f7987e701cfc916f6e31b35
Max Schneider <mschneider30@stuy.edu>
2021-10-04 21:01:02 -0400

    Update greet.java

--- 07_helloclass/greet.java
+++ 07_helloclass/greet.java
@@ -1,3 +1,9 @@
+/*
+Max Schneider
+APCS
+HW 7 -- Saving Your Class/Greet.java/created Greet.java file
+2021 09 30
+*/
 public class greet {
 	public static void main(String[] args) {
 		System.out.println("No hablo queso");
