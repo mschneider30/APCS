dc31a5a2c89f975a68eeaef066970ce733e28e8d
Max Schneider <mschneider30@stuy.edu>
2021-10-05 18:56:45 -0400

    Update BigSib.java

--- 12_bigsib/BigSib.java
+++ 12_bigsib/BigSib.java
@@ -1,6 +1,3 @@
-/* I discovered how to get a method to return a string. I am still wondering why a method can't return different types of variables.
-*/
-
 public class BigSib {
 	static String greet(String s) {
 		return ("HelloMsg" + "greeting");
