4cec7709c3f3029252f0935b6b3375d580b81a4c
Max Schneider <mschneider30@stuy.edu>
2021-10-07 19:09:39 -0400

    Update BigSib.java

--- 14_overload/defcon/BigSib.java
+++ 14_overload/defcon/BigSib.java
@@ -2,11 +2,6 @@ public class BigSib {
 	String HelloMsg;
 	public BigSib (String S){
 		HelloMsg = S;
-}
-	public void setHelloMsg(String s){
-		if (HelloMsg == "Word up"){
-			HelloMsg=s;
-		}
 	}
 	String greet(String ss) {
 		ss=(HelloMsg+' '+ss);
