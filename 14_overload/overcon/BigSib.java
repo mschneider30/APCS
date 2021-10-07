aea9cce4b012cb803af4c8a76e3d66e803bcfb00
Max Schneider <mschneider30@stuy.edu>
2021-10-07 19:17:15 -0400

    Update BigSib.java

--- 14_overload/overcon/BigSib.java
+++ 14_overload/overcon/BigSib.java
@@ -2,10 +2,10 @@ public class BigSib {
 	String HelloMsg;
 	String Person;
 	public BigSib (){
-		Person richard = "freshman" ;
-		Person grizz = "Dr. Spaceman" ;
-		Person dotCom = "Kong Fooey" ;
-		Person tracy = "mom" ;
+		Person.richard = "freshman" ;
+		Person.grizz = "Dr. Spaceman" ;
+		Person.dotCom = "Kong Fooey" ;
+		Person.tracy = "mom" ;
 }
 	String greet(String ss) {
 		ss=(HelloMsg+' '+ss);
