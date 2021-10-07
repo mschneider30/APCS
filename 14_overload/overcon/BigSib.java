93c222d4948d84bb0d4c091059403a30cec38b2f
Max Schneider <mschneider30@stuy.edu>
2021-10-07 19:27:38 -0400

    Update BigSib.java

--- 14_overload/overcon/BigSib.java
+++ 14_overload/overcon/BigSib.java
@@ -1,11 +1,20 @@
 public class BigSib {
 	String HelloMsg;
-	String Person;
-	public BigSib (){
-		Person.richard = "freshman" ;
-		Person.grizz = "Dr. Spaceman" ;
-		Person.dotCom = "Kong Fooey" ;
-		Person.tracy = "mom" ;
+	if (BigSib == richard){
+		HelloMsg = "Word up;
+	}
+	else if (BigSib == grizz){
+		HelloMsg = "Hey ya";
+	}
+	else if (BigSib == dotCom){
+		HelloMsg = "Sup";
+	}
+	else if (BigSib == tracy){
+		HelloMsg = "Salutations";
+	}
+	else {
+		HelloMsg= "No way, its you!";
+	}
 }
 	String greet(String ss) {
 		ss=(HelloMsg+' '+ss);
