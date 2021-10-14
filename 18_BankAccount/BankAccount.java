d0c437493395e3bd154f8ec5b793e99d9645239f
Max Schneider <mschneider30@stuy.edu>
2021-10-14 19:01:14 -0400

    Update BankAccount.java

--- 18_BankAccount/BankAccount.java
+++ 18_BankAccount/BankAccount.java
@@ -1,10 +1,16 @@
 public class BankAccount {
-	private String fullName;
-	private String password;
-	private int pin;
-	private int accountNumber;
-	private float balance;
-
+	
+	String fullName;
+	String password;
+	int pin;
+	int accountNumber;
+	float balance;
+	public BankAccount(String S, String T, int a, int b, float f);
+		fullName = S;
+		password = T;
+		pin = a;
+		accountNumber = b;
+		balance = f;
 	public void setValues(String S, String T, int a, int b, float f){
 		fullName = S;
 		password = T;
@@ -13,19 +19,19 @@ public class BankAccount {
 		balance = f; 
 	}
 
-	public void withdraw(float g, int aa){
-		setValues(fullName, password, pin, aa, balance - g);
+	public void withdraw(float g){
+		balance = balance - g;
 	}
 	public void deposit(float h, int bb) {
-		setValues(fullName, password, pin, bb, balance + h);
+		balance = balance - h;
 	}
 	public void actInfo (String[] args){
 		System.out.println(fullName + password + pin + accountNumber + balance);
 	}
 	public void main(String S0, String T0, int a0, int b0, float f0){
 		setValues(S0, T0, a0, b0, f0);
-		withdraw(100, a0);
-		deposit(200, a0);
+		withdraw(100);
+		deposit(200);
 		actInfo("");
 	}
 }
