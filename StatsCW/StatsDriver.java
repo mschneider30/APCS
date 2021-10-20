15de5190d81d59ea1a13a7faecce613561604774
mschneider30 <mschneider30@stuy.edu>
2021-10-20 12:39:34 -0400

    Added 10/20/21 classwork

---
+++ StatsCW/StatsDriver.java
@@ -0,0 +1,25 @@
+public class StatsDriver {
+
+	public static void main(String[] args) {
+		int a
+		a=10
+		int a2
+		b=40
+		double c
+		c=10.00
+		double c2
+		c2=40.00
+		meanOutput=mean(a,a2);
+		doublemeanOutput=mean(c,c2);
+		maxOutput=max(a,a2);
+		doublemaxOutput=max(c,c2);
+		geoMeanOutput=geoMean(a,a2);
+		doublegeoMeanOutput=geoMean(c,c2);
+		System.out.println("Correct Output:25   Actual Output: " meanOutput);
+		System.out.println("Correct Output: 25.0   Actual Output: " doublemeanOutput);
+		System.out.println("Correct Output: 40    Actual Output: " maxOutput);
+		System.out.println("Correct Output: 40.0   Actual Output: " doublemaxOutput);
+		System.out.println("Correct Output: 20    Actual Output: " geoMeanOutPut);
+		System.out.println("Correct Output: 20.0   Actual Output: " doublegeoMeanOutput
+	}
+} 
