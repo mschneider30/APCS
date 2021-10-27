48c424b02b24c82fcf10e23c42bf4ce5636179d9
Max Schneider <mschneider30@stuy.edu>
2021-10-27 18:53:29 -0400

    Update Stats.java

--- 26_gcd/Stats.java
+++ 26_gcd/Stats.java
@@ -18,17 +18,17 @@ public class Stats {
         }
       return answer;
   }
-  public static int gdcER (int a , int b) {
+  public static int gcdER (int a , int b) {
       if (a==b) {
         return a;
       }
       else if (a>b) {
-        return gdcER(a-b,b);
+        return gcdER(a-b,b);
       } else {
-        return gdcER(a,b-a);
+        return gcdER(a,b-a);
         }
   }
-  public static int gdcEW (int a, int b) {
+  public static int gcdEW (int a, int b) {
       while (a!=b) {
         if (a > b) {
           a-=b;
@@ -40,27 +40,27 @@ public class Stats {
   }
     
   public static void main(String[] args) {
-    /*
-      System.out.println("gdc method: Should print 1, 3, 1, 112");
+    
+      System.out.println("gcd method: Should print 1, 3, 1, 112");
       Stats Stat1 = new Stats();
-      System.out.println(Stat1.gdc(1,3));
-      System.out.println(Stat1.gdc(3,9));
-      System.out.println(Stat1.gdc(213,214));
-      System.out.println(Stat1.gdc(112,224));
-     */                    
-      System.out.println("gdcER method: Should print 1, 3, 1, 112");
+      System.out.println(Stat1.gcd(1,3));
+      System.out.println(Stat1.gcd(3,9));
+      System.out.println(Stat1.gcd(213,214));
+      System.out.println(Stat1.gcd(112,224));
+                       
+      System.out.println("gcdER method: Should print 1, 3, 1, 112");
       Stats Stat2 = new Stats();
-      System.out.println(Stat2.gdcER(1,3));
-      System.out.println(Stat2.gdcER(3,9));
-      System.out.println(Stat2.gdcER(213,214));
-      System.out.println(Stat2.gdcER(112,224));
+      System.out.println(Stat2.gcdER(1,3));
+      System.out.println(Stat2.gcdER(3,9));
+      System.out.println(Stat2.gcdER(213,214));
+      System.out.println(Stat2.gcdER(112,224));
                          
-      System.out.println("gdcEW method: Should print 1, 3, 1, 112");
+      System.out.println("gcdEW method: Should print 1, 3, 1, 112");
       Stats Stat3 = new Stats();
-      System.out.println(Stat3.gdcEW(1,3));
-      System.out.println(Stat3.gdcEW(3,9));
-      System.out.println(Stat3.gdcEW(213,214));
-      System.out.println(Stat3.gdcEW(112,224));
+      System.out.println(Stat3.gcdEW(1,3));
+      System.out.println(Stat3.gcdEW(3,9));
+      System.out.println(Stat3.gcdEW(213,214));
+      System.out.println(Stat3.gcdEW(112,224));
     
   }
 }
