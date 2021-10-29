c9945e9a2e4bf410d526d3088702904646ec8347
Max Schneider <mschneider30@stuy.edu>
2021-10-28 22:16:43 -0400

    Update Rof.java

--- 27_forrevstr/Rof.java
+++ 27_forrevstr/Rof.java
@@ -7,9 +7,30 @@ public class Rof {
           return fence;
           
     }
+    
+    public static String reverseF(String s) {
+            if (s=="") {
+                    System.out.println("Please enter non-empty string");
+            }
+            int sLength = length(s);
+            String reversed = "";
+            int index = sLength - i;
+            for(int i = 0; i < sLength; i++) {
+                    String newLetter = s.substring(index,index-1)
+                    reversed+=newLetter
+    }
+    
     public static void main(String[] args) {
+        System.out.println("Fence Testing: Should return 1 post then 2 posts then 3 posts");
         System.out.println(fenceF(1));
         System.out.println(fenceF(2));
         System.out.println(fenceF(3));
+        
+        System.out.println("ReverseF testing: Should return "desserts" then "tnasevyutS" then an error message asking for a non-empty string");
+        System.out.println(reverseF("stressed"));
+        System.out.println(reverseF("Stuyvesant"));
+        System.out.println(reverseF(""));
+        
+        
     }
 }
