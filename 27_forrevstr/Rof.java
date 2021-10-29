199723408c6b9fda00d50fce632150ef8ed7ee4f
Max Schneider <mschneider30@stuy.edu>
2021-10-28 22:04:16 -0400

    Create Rof.java

---
+++ 27_forrevstr/Rof.java
@@ -0,0 +1,14 @@
+public class Fences {
+    public static String fenceF(int posts) {
+          String fence = "|";
+          for (int i = 1;i < posts; i++) {
+               fence+="--|";
+          return fence;
+          }
+    }
+    public static void main(String[] args) {
+        System.out.println(fenceF(1));
+        System.out.println(fenceF(2));
+        System.out.println(fenceF(3));
+    }
+}
