bcef202d8cbe9629f8b0c25ebe07e52111cbac36
Max Schneider <mschneider30@stuy.edu>
2021-10-30 18:09:30 -0400

    Update and rename helloName to helloName.java

---
+++ cb/string-1/helloName.java
@@ -0,0 +1,11 @@
+public class helloName{
+  public String helloName(String name) {
+  String greeting = "Hello " + name +"!";
+  return greeting;
+  }
+  public static void main(String[] args) {
+    System.out.println(helloName("Bob"));
+    System.out.println(helloName("Alice"));
+    System.out.println(helloName("X"));
+  }
+}
