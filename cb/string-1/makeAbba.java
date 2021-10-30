553579b00272442c62c8355bb9ae7d778ef33c0f
Max Schneider <mschneider30@stuy.edu>
2021-10-30 18:12:56 -0400

    Update and rename makeAbba to makeAbba.java

---
+++ cb/string-1/makeAbba.java
@@ -0,0 +1,11 @@
+public class makeAbba{
+  public String makeAbba(String a, String b) {
+  String greeting = a+b+b+a;
+  return greeting;
+}
+  public static void main(String[] args) {
+    System.out.println(makeAbba("hi", "Bye"));
+    System.out.println(makeAbba("Yo", "Alice"));
+    System.out.println(makeAbba("What", "Up"));
+  }
+}
