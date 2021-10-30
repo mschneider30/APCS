ff31052aeb41eac164aa6e4b20a190d443c2239f
Max Schneider <mschneider30@stuy.edu>
2021-10-30 19:00:39 -0400

    Update catDog.java

--- cb/string-2/catDog.java
+++ cb/string-2/catDog.java
@@ -1,6 +1,22 @@
 public class {
   
-  
+  public boolean catDog(String str) {
+  int cat = 0;
+  int dog = 0;
+  for (int i = 0;i < str.length()-2;i+=1) {
+    String letters = str.substring(i,i+3);
+    if (letters.equals("cat")) {
+      cat++;
+    } else if (letters.equals("dog")) {
+      dog++;
+    }
+  }
+  if (dog == cat) {
+    return true;
+  } else {
+    return false;
+  }
+}
   
   public static void main(String args[]) {
     system.out.println();
