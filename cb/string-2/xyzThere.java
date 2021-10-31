74341ed6d94475366cf1afb52c458fa132e32880
Max Schneider <mschneider30@stuy.edu>
2021-10-30 21:12:41 -0400

    Update xyzThere.java

--- cb/string-2/xyzThere.java
+++ cb/string-2/xyzThere.java
@@ -1,10 +1,23 @@
 public class {
   
-  
+  public boolean xyzThere(String str) {
+  for (int i = 0;i < str.length()-2;i++) {
+    String Letters = str.substring(i,i+3);
+    if (Letters.equals("xyz")) {
+      if (i==0) {
+        return true;
+      } else if (!(str.substring(i-1,i).equals("."))){
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
   
   public static void main(String args[]) {
-    system.out.println();
-    system.out.println();
-    system.out.println();
+    system.out.println(xyzThere("abcxyz"));
+    system.out.println(xyzThere("abc.xyz"));
+    system.out.println(xyzThere("xyz.abc"));
   }
 }
