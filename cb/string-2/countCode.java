f72a6c1ad77036d02488c4c0ab84e2848f0e9d84
Max Schneider <mschneider30@stuy.edu>
2021-10-30 20:50:26 -0400

    Update countCode.java

--- cb/string-2/countCode.java
+++ cb/string-2/countCode.java
@@ -1,10 +1,21 @@
 public class {
   
-  
+  public int countCode(String str) {
+  int counter = 0;
+  for (int i = 0; i < str.length()-3; i++) {
+    String letters = str.substring(i,i+2);
+    String letters2 = str.substring(i+3,i+4);
+    if (letters.equals("co") && letters2.equals("e")) {
+      counter++;
+    }
+  }
+  return counter;
+}
+
   
   public static void main(String args[]) {
-    system.out.println();
-    system.out.println();
-    system.out.println();
+    system.out.println(countCode("aaacodebbb"));
+    system.out.println(countCode("codexxdoce"));
+    system.out.println(countCode("cozexxcope"));
   }
 }
