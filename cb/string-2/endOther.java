3b54fcc087ef32d1fd6c814431d6e61c896653f7
Max Schneider <mschneider30@stuy.edu>
2021-10-30 21:03:41 -0400

    Update endOther.java

--- cb/string-2/endOther.java
+++ cb/string-2/endOther.java
@@ -1,10 +1,26 @@
 public class {
   
-  
+  public boolean endOther(String a, String b) {
+  if (a.length() > b.length()) {
+  String Letters2 = a.substring(a.length()-b.length(),a.length());
+  Letters2 = Letters2.toLowerCase();
+  if (Letters2.equals(b.toLowerCase())) {
+    return true;
+  }
+  }
+  else {
+  String Letters = b.substring(b.length()-a.length(),b.length());
+  Letters = Letters.toLowerCase();
+  if (Letters.equals(a.toLowerCase())) {
+    return true;
+  }
+  }
+  return false;
+}
   
   public static void main(String args[]) {
-    system.out.println();
-    system.out.println();
-    system.out.println();
+    system.out.println(endOther("Hiabc", "abc"));
+    system.out.println(endOther("AbC", "HiaBc"));
+    system.out.println(endOther("abc","abXabc"));
   }
 }
