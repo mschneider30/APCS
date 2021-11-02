60e976c6429deba083acb37b983abcab42583874
Max Schneider <mschneider30@stuy.edu>
2021-11-02 18:00:30 -0400

    Update Commafier.java

--- 29_cliforeastr/Commafier.java
+++ 29_cliforeastr/Commafier.java
@@ -1,3 +1,18 @@
 public class Commafier {
-
+  public static String commafyF(int a) {
+    String answer="";
+    String [] newStuff = Integer.toString(a);
+    int n = 0;
+    for (String s : newStuff) {
+      n+=1;
+      if (n % 3 == 0) {
+        answer = "," + s + answer;
+      } else {
+        answer = s + answer;
+      }
+   }
+   return answer;
+  }
+  public staic void main(String[] args) {
+    System.out.println(commafyF(10000000));
 }
