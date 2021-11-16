e8c46ed7d58d23c246d0ae91116d0f463ce32e0f
Max Schneider <mschneider30@stuy.edu>
2021-11-16 13:19:25 -0500

    Update firstLast6.java

--- cb/Array-1/firstLast6.java
+++ cb/Array-1/firstLast6.java
@@ -2,7 +2,7 @@ public class firstLast6 {
     //Given an array of ints, return true if 6 appears as either the first or last element in the array. The array will be length 1 or more.
     public static boolean firstLast6(int[] nums) {
     if (nums[0] == 6 || nums[nums.length-1] == 6) {
-      return true;
+      return true; 
     } else {
       return false;
     }
