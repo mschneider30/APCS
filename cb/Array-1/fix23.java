d4fe761b22339e07192dac7f59b2c1282c436f86
Max Schneider <mschneider30@stuy.edu>
2021-11-16 13:19:35 -0500

    Update fix23.java

--- cb/Array-1/fix23.java
+++ cb/Array-1/fix23.java
@@ -1,7 +1,7 @@
 public class fix23 {
   public int[] fix23(int[] nums) {
     if (nums[0] == 2 && nums[1] == 3) {
-      nums[1] = 0;
+      nums[1] = 0; 
     }
     if (nums[1] == 2 && nums[2] == 3) {
       nums[2] = 0;
