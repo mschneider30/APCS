3387a25cd78005990e6172c1bc957f57492b0e5d
mschneider30 <mschneider30@stuy.edu>
2021-12-02 13:19:27 -0500

    Added 42

---
+++ 42_rat/rational.java
@@ -0,0 +1,89 @@
+public class Rational {
+  private int numerator;
+  private int denominator;
+
+  public Rational (){
+    numerator = 0;
+    denominator = 1;
+  }
+  public Rational (int a, int b) {
+    numerator=a;
+    denominator=b;
+  }
+
+  public String toString() {
+    return ("(" + numerator + ", " + denominator + ")");
+  }
+  public float floatValue () {
+    return (float)numerator/(float)denominator;
+  }
+  public void multiply (Rational r) {
+    this.numerator=this.numerator*r.numerator;
+    this.denominator=this.denominator*r.denominator;
+  }
+  public void divide (Rational r) {
+    this.numerator=this.numerator*r.denominator;
+    this.denominator=this.denominator*r.numerator;
+  }
+
+  public void add (Rational r) {
+    this.numerator*=r.denominator;
+    this.denominator*=r.denominator;
+    r.numerator*=this.denominator;
+    r.denominator*=this.denominator;
+    this.numerator+=r.numerator;
+  }
+
+
+  public void subtract (Rational r) {
+    this.numerator*=r.denominator;
+    this.denominator*=r.denominator;
+    r.numerator*=this.denominator;
+    r.denominator*=this.denominator;
+    this.numerator-=r.numerator;
+  }
+
+  public int GCD () {
+    int a = this.numerator;
+    int b = this.denominator;
+    int c = 1;
+    int answer = 0;
+    while (c <= a) {
+      if (a%c==0 && b%c==0) {
+        answer=c;
+        }
+      c++;
+      }
+    return answer;
+  }
+
+  public void reduce () {
+    int divisor = this.GCD();
+    this.numerator /= divisor;
+    this.denominator /= divisor;
+  }
+
+  public int compareTo (Rational R) {
+    return (int)a;
+  }
+
+  public static void main(String[] args) {
+    Rational r1 = new Rational();
+    Rational r2 = new Rational(1,2);
+    Rational r3 = new Rational(1,3);
+
+    System.out.println(r1.toString());
+    System.out.println(r2.toString());
+    System.out.println(r3.toString());
+
+    System.out.println(r1.floatValue());
+    System.out.println(r2.floatValue());
+    System.out.println(r3.floatValue());
+
+    r2.multiply(r3);
+    System.out.println(r2.toString());
+
+    r3.divide(r2);
+    System.out.println(r3.toString());
+  }
+}
