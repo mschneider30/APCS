f12648aa4adf58598b198b43d8c0beea086773e1
Max Schneider <mschneider30@stuy.edu>
2021-12-08 23:55:18 -0500

    Update SuperArray.java

--- 45_suparr/SuperArray.java
+++ 45_suparr/SuperArray.java
@@ -1,7 +1,7 @@
 // Blue Steaks - Max Schneider, Faiyaz Rafee, David Deng
 // APCS1 pd7
-// HW44 -- expanding SuperArray functionality, encapsulation
-// 2021-12-08w
+// HW45 -- Array of Titanium / Expanding SuperArray functionality, interfaces
+// 2021-12-09
 
 /***************************
  * class SuperArray version 2.0
@@ -13,65 +13,56 @@
  * adding an element to end of array
  * adding an element at specified index
  * removing an element at specified index
-
  Disco:
- *Importance of .this. Code did not work otherwise.
- *JAVA VISUALIZER!!!
- *the size() method is very helpful and needs to be utitlized anytime a we chnage the number of meaning ful values in the arrays
-
+ *Documentation helps debugging code
  QCCs:
- How much less inefficient is a deep copy?
+ *What practical applications does an interface have?
  ***************************/
 
 public class SuperArray implements ListInt
 {
 
-  private int[] _data;  //underlying container
+  private int[] _data;  //underlying ("encapsulated") container
   private int _size;    //number of elements in this SuperArray
 
 
   //default constructor – initializes 10-item array
   public SuperArray()
   {
-    this._data = new int[10];
-    this._size = 0;
+    _data = new int[10];
+    _size = 0;
   }
 
 
   //output SuperArray in [a,b,c] format
   public String toString()
   {
-    this._size=size();
-    String outVal = "[";
-    for (int i = 0; i < this._size ; i++) {
-	    if (i == this._size-1) {
-		    outVal+=this._data[i];
-	    } else {
-		    outVal+=(this._data[i]+", ");
-	    }
-
+    String foo = "[";
+    for( int i = 0; i < _size; i++ ) {
+      foo += _data[i] + ",";
     }
-    outVal+="]";
-    return outVal;
+    if ( foo.length() > 1 )
+      //shave off trailing comma
+      foo = foo.substring( 0, foo.length()-1 );
+    foo += "]";
+    return foo;
   }
 
 
   //double capacity of SuperArray
   private void expand()
   {
-    int[] newArray = new int[this._size * 2];
-    for (int i = 0;i < this._size; i++) {
-      newArray[i]=this._data[i];
-    }
-    this._data=newArray;
-    this._size *=2;
+    int[] temp = new int[ _data.length * 2 ];
+    for( int i = 0; i < _data.length; i++ )
+      temp[i] = _data[i];
+    _data = temp;
   }
 
 
   //accessor -- return value at specified index
   public int get( int index )
   {
-    return this._data[index];
+    return _data[index];
   }
 
 
@@ -79,22 +70,22 @@ public class SuperArray implements ListInt
   //           return old value at index
   public int set( int index, int newVal )
   {
-    int answer = get(index);
-    this._data[index]=newVal;
-    if (index > this._size) {
-      this._size++;
-    }
-    return answer;
+    int temp = _data[index];
+    _data[index] = newVal;
+    return temp;
   }
 
 
   //adds an item after the last item
   public boolean add( int newVal )
   {
+    //checks if current array is large enough
     if (this._size == this._data.length) {
       expand();
     }
+    //appends new value to end of subarray of meaningful values
     this._data[this._size]=newVal;
+    //increments length of meaningful values
     this._size++;
     return true;
   }
@@ -103,13 +94,17 @@ public class SuperArray implements ListInt
   //inserts an item at index
   public boolean add( int index, int newVal )
   {
+    //checks if current array is large enough
     if (this._size >= this._data.length) {
       expand();
     }
+    //shifts all values right of index where insertion will happen to the right by 1
     for(int i = this._size; i>index;i--) {
       this._data[i]=this._data[i-1];
     }
+    //inserts newVal at allocated index
     this._data[index]=newVal;
+    //increments length of meaningful values
     this._size++;
     return true;
   }
@@ -119,10 +114,13 @@ public class SuperArray implements ListInt
   //shifts elements left to fill in newly-empted slot
   public void remove( int index )
   {
+    //shifts all values right of index where value will be removed to the left by 1
     for(int i = index; i<this._size-1;i++) {
       this._data[i]=this._data[i+1];
     }
+    //sets far right value of previous subarray of meaningful values to 0
     this._data[this._size-1]=0;
+    //decrements length of meaningful values
     this._size--;
   }
 
@@ -130,13 +128,18 @@ public class SuperArray implements ListInt
   //return number of meaningful items in _data
   public int size()
   {
+    //sets meaningful items to length of array
     int answer = this._data.length;
+    //iterates from the back 
     for (int i = this._data.length-1; this._data[i] == 0; i--) {
+      //decrements number of meaningful values
       answer--;
+      //accounts for case when there are no meaningful values
       if (i == 0) {
         return answer;
       }
     }
+    //returns number of meaningful values should the iteration encounter one
     return answer;
   }
 
@@ -145,25 +148,24 @@ public class SuperArray implements ListInt
   //main method for testing
   public static void main( String[] args )
   {
-    SuperArray curtis = new SuperArray();
-    System.out.println( "Printing empty SuperArray curtis..." );
-    System.out.println( curtis );
+      SuperArray curtis = new SuperArray();
+      System.out.println( "Printing empty SuperArray curtis..." );
+      System.out.println( curtis );
 
-    for( int i = 0; i < curtis._data.length; i++ ) {
-    curtis.set( i, i * 2 );
-    }
+      for( int i = 0; i < curtis._data.length; i++ ) {
+      curtis.set( i, i * 2 );
+      }
 
-    System.out.println("Printing populated SuperArray curtis...");
-    System.out.println(curtis);
+      System.out.println("Printing populated SuperArray curtis...");
+      System.out.println(curtis);
 
-    for( int i = 0; i < 3; i++ ) {
-    curtis.expand();
-    for( int b = 0; b < curtis._size; b++ ) {
-    curtis.set( b, (int)(Math.random()*100) );
-    }
-    System.out.println("Printing expanded SuperArray curtis...");
-    System.out.println(curtis);
-    }
+      for( int i = 0; i < 3; i++ ) {
+      curtis.expand();
+      System.out.println("Printing expanded SuperArray curtis...");
+      System.out.println(curtis);
+      System.out.println("new length of underlying array: "
+      + curtis._data.length );
+      }
 
       SuperArray mayfield = new SuperArray();
       System.out.println("Printing empty SuperArray mayfield...");
@@ -194,6 +196,8 @@ public class SuperArray implements ListInt
       mayfield.add(1,77);
       System.out.println("Printing SuperArray mayfield post-insert...");
       System.out.println(mayfield);
+      /*~~~~~~~~move~me~down~~~~~~~~~~~~~~V~~~~~~~~
+      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|~~~~~~~~*/
   }//end main()
 
 
