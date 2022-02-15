4b08b5f2e60265949fd1ee173aa04d7fca25e8f9
Max Schneider <Max_Schneider@iMac.local>
2022-02-14 22:23:49 -0500

    Did a whole lot of recursion

--- cb/recursion-2/GroupSum.java
+++ cb/recursion-2/GroupSum.java
@@ -3,7 +3,7 @@
 // 2022-2-15
 // time elapsed: _hrs
  public class GroupSum {
-   public boolean groupSum(int start, int[] nums, int target) {
+   public static boolean groupSum(int start, int[] nums, int target) {
      if (start >= nums.length) {
        return target == 0;
      }
@@ -17,8 +17,12 @@
 
    }
   public static void main(String[] args) {
-   System.out.println(groupSum(0, [2,4,8], 10));
-   System.out.println(groupSum(0, [2,4,8], 14));
-   System.out.println(groupSum(0, [2,4,8], 9));
+    int[] r = new int[3];
+    r[0]=2;
+    r[1]=4;
+    r[2]=8;
+   System.out.println(groupSum(0, r, 10));
+   System.out.println(groupSum(0, r, 14));
+   System.out.println(groupSum(0, r, 9));
   }
  }
