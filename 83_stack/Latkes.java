28b279bd2c4a78a6c190df4124cbb39150859c20
mschneider30 <mschneider30@stuy.edu>
2022-03-28 22:40:39 -0400

    Latkes are delicious

--- 83_stack/Latkes.java
+++ 83_stack/Latkes.java
@@ -1,3 +1,9 @@
+// Team Pom Pom: Max Schneider, Perry Huang, Oscar Breen
+// HW83 -- Stacks on Stacks 
+// APCS
+// 2022-03-28
+// Time spent: 0.5 hrs
+
 /***
  * class Latkes
  * v1
@@ -7,8 +13,11 @@
 
 /***
     DISCO
+    This class is pretty efficient because all the methods are O(1).
 
     QCC
+    Is it possible to make a pop method that removes at index 0 that is more efficient?
+    Are all of these methods actually O(1)?
 
  **/
 
@@ -24,44 +33,49 @@ public class Latkes
   {
     _stackSize = 0;
     _stack = new String[initCapacity];
-  }// O(?)
+  }// O(1)
 
 
   //means of insertion
   public void push( String s )
   {
-    if (!isFull()) {
-      _stack[_stackSize] = s;
-      _stackSize++;
+    if (isFull()) {
+    System.out.println("Nope, we've reached max capacity.");
+    return;
     }
-  }// O(?)
+    _stack[_stackSize] = s;
+    _stackSize++;
+    System.out.println("Added " + s);
+  }// O(1)
 
 
   //means of removal
   public String pop( )
   {
-    if (!isEmpty()) {
-      String r = _stack[_stackSize - 1];
-      _stack[_stackSize - 1] = null;
-      _stackSize--;
-      return r;
+    if (isEmpty()) {
+        return "Nope, we've run out";
     }
-  return "Nope, not valid";
-  }// O(?)
+    String r = _stack[_stackSize - 1];
+    _stack[_stackSize - 1] = null;
+    _stackSize--;
+    System.out.print("Removed ");
+    return r;
+
+  }// O(1)
 
 
-  //chk for emptiness
+  //check for emptiness
   public boolean isEmpty()
   {
     return (_stackSize == 0);
-  }// O(?)
+  }// O(1)
 
 
   //chk for fullness
   public boolean isFull()
   {
     return (_stackSize == _stack.length);
-  }// O(?)
+  }// O(1)
 
 
   //main method for testing
