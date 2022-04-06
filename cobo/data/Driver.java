ac0362342233a3baeb2e4da3846040eadb6ea7f7
mschneider30 <mschneider30@stuy.edu>
2022-04-06 12:45:50 -0400

    d

--- cobo/data/Driver.java
+++ cobo/data/Driver.java
@@ -7,7 +7,9 @@ public class Driver {
       ds1.setCacheTimeout(15 * 60);
       ds1.load();
       //ds1.printUsageString();
-
+      float temp = ds.fetchFloat("temp_f");
+      String loc = ds.fetchString("location");
+      System.out.println("The temperature at " + loc + " is " + temp + "F");
 
 
    }
